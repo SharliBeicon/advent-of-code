f770e67fa002c32d51ba32352a9f8643e1048944
Charlie Bacon <ct@databeacon.aero>
2024-12-08 00:35:32 +0100

    day07

--- src/day07.zig
+++ src/day07.zig
@@ -1,12 +1,15 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
 const List = std.ArrayList;
 const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
 
 const util = @import("util.zig");
 const gpa = util.gpa;
+
+const print = std.debug.print;
+const tokenizeSca = std.mem.tokenizeScalar;
+const splitSca = std.mem.splitScalar;
+const parseInt = std.fmt.parseInt;
+const allocPrint = std.fmt.allocPrint;
 
 const data = @embedFile("data/day07.txt");
 
@@ -67,42 +70,11 @@ fn isValidCalibrationiWithConcat(target: u64, operands: []u64, current: u64, ind
     if (index >= operands.len - 1) return target == current;
 
     const new_index = index + 1;
-    const str1 = try std.fmt.allocPrint(gpa, "{d}", .{current});
-    const str2 = try std.fmt.allocPrint(gpa, "{d}", .{operands[new_index]});
+    const str1 = try allocPrint(gpa, "{d}", .{current});
+    const str2 = try allocPrint(gpa, "{d}", .{operands[new_index]});
     const concated = try std.mem.concat(gpa, u8, &[_][]const u8{ str1, str2 });
     const concated_num = try parseInt(u64, concated, 10);
     return try isValidCalibrationiWithConcat(target, operands, current + operands[new_index], new_index) or
         try isValidCalibrationiWithConcat(target, operands, current * operands[new_index], new_index) or
         try isValidCalibrationiWithConcat(target, operands, concated_num, new_index);
 }
-
-// Useful stdlib functions
-const tokenizeAny = std.mem.tokenizeAny;
-const tokenizeSeq = std.mem.tokenizeSequence;
-const tokenizeSca = std.mem.tokenizeScalar;
-const splitAny = std.mem.splitAny;
-const splitSeq = std.mem.splitSequence;
-const splitSca = std.mem.splitScalar;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
-
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
-
-const print = std.debug.print;
-const assert = std.debug.assert;
-
-const sort = std.sort.block;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
-
-// Generated from template/template.zig.
-// Run `zig build generate` to update.
-// Only unmodified days will be updated.
