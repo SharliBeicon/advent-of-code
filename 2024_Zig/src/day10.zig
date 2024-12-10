56c816d05bd47edee25ce44e7630fcd744f5ccbf
Charlie Bacon <ct@databeacon.aero>
2024-12-10 20:13:51 +0100

    day 10

--- 2024_Zig/src/day10.zig
+++ 2024_Zig/src/day10.zig
@@ -1,46 +1,123 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
 const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
 
 const util = @import("util.zig");
 const gpa = util.gpa;
 
+const tokenizeSca = std.mem.tokenizeScalar;
+const parseInt = std.fmt.parseInt;
+const print = std.debug.print;
+
 const data = @embedFile("data/day10.txt");
 
+const step_direction = [_]@Vector(2, isize){
+    .{ -1, 0 }, // UP
+    .{ 0, 1 }, // RIGHT
+    .{ 1, 0 }, // DOWN
+    .{ 0, -1 }, // LEFT
+};
+
 pub fn main() !void {
-    
+    var map_list = List([]const u8).init(gpa);
+    var trailheads_list = List(@Vector(2, isize)).init(gpa);
+    var visited_grid = List([]bool).init(gpa);
+    var it = tokenizeSca(u8, data, '\n');
+    var i: isize = 0;
+
+    while (it.next()) |row| {
+        try map_list.append(row);
+        var visited_list = List(bool).init(gpa);
+
+        for (row, 0..) |item, j| {
+            if (item == '0') {
+                try trailheads_list.append(.{ i, @intCast(j) });
+            }
+            try visited_list.append(false);
+        }
+        try visited_grid.append(try visited_list.toOwnedSlice());
+        i += 1;
+    }
+    const map01 = try map_list.toOwnedSlice();
+    const map02 = try std.mem.Allocator.dupe(gpa, []const u8, map01);
+    for (map01, 0..) |row, mi| {
+        map02[mi] = try std.mem.Allocator.dupe(gpa, u8, row);
+    }
+
+    map_list.deinit();
+    const trailheads = try trailheads_list.toOwnedSlice();
+    trailheads_list.deinit();
+    var visited = try visited_grid.toOwnedSlice();
+    visited_grid.deinit();
+
+    var part01: u32 = 0;
+    var part02: u32 = 0;
+    for (trailheads) |th| {
+        for (visited) |row| {
+            @memset(row, false);
+        }
+        part01 += try getTrailsPart01(map01, th, 0, &visited);
+    }
+    for (trailheads) |th| {
+        for (visited) |row| {
+            @memset(row, false);
+        }
+        part02 += try getTrailsPart02(map02, th, 0, &visited);
+    }
+
+    std.debug.print("***DAY 10***\nPart 01: {}\nPart 02: {}\n\n", .{ part01, part02 });
 }
 
-// Useful stdlib functions
-const tokenizeAny = std.mem.tokenizeAny;
-const tokenizeSeq = std.mem.tokenizeSequence;
-const tokenizeSca = std.mem.tokenizeScalar;
-const splitAny = std.mem.splitAny;
-const splitSeq = std.mem.splitSequence;
-const splitSca = std.mem.splitScalar;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
+fn getTrailsPart01(map: [][]const u8, trailhead: @Vector(2, isize), next_expected: isize, visited: *[][]bool) !u32 {
+    if (!util.inBounds(map, trailhead)) return 0;
 
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
+    const current = try parseInt(
+        isize,
+        &[_]u8{map[@intCast(trailhead[0])][@intCast(trailhead[1])]},
+        10,
+    );
 
-const print = std.debug.print;
-const assert = std.debug.assert;
+    if (visited.*[@intCast(trailhead[0])][@intCast(trailhead[1])] or
+        current != next_expected)
+        return 0;
 
-const sort = std.sort.block;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
+    visited.*[@intCast(trailhead[0])][@intCast(trailhead[1])] = true;
+    if (current == 9) return 1;
 
-// Generated from template/template.zig.
-// Run `zig build generate` to update.
-// Only unmodified days will be updated.
+    var trails: u32 = 0;
+    for (step_direction) |step| {
+        const next_step = trailhead + step;
+
+        trails += try getTrailsPart01(map, next_step, current + 1, visited);
+    }
+
+    visited.*[@intCast(trailhead[0])][@intCast(trailhead[1])] = false;
+    return trails;
+}
+
+fn getTrailsPart02(map: [][]const u8, trailhead: @Vector(2, isize), next_expected: isize, visited: *[][]bool) !u32 {
+    if (!util.inBounds(map, trailhead)) return 0;
+
+    const current = try parseInt(
+        isize,
+        &[_]u8{map[@intCast(trailhead[0])][@intCast(trailhead[1])]},
+        10,
+    );
+
+    if (visited.*[@intCast(trailhead[0])][@intCast(trailhead[1])] or
+        current != next_expected)
+        return 0;
+
+    if (current == 9) return 1;
+
+    var trails: u32 = 0;
+    for (step_direction) |step| {
+        const next_step = trailhead + step;
+
+        visited.*[@intCast(trailhead[0])][@intCast(trailhead[1])] = true;
+
+        trails += try getTrailsPart02(map, next_step, current + 1, visited);
+    }
+
+    visited.*[@intCast(trailhead[0])][@intCast(trailhead[1])] = false;
+    return trails;
+}
